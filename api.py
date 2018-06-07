88e59401b2d67211ccc9b7c53837274a736c3f5d
gtsofa <jlsnyule@yahoo.com>
2018-06-07 14:24:56 +0300

    fix get_a_user method's route

--- api.py
+++ api.py
@@ -1,25 +1,31 @@
 # api.py
+import os
 from flask import Flask, request, jsonify
 
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask_sqlachemy import SQLAlchemy
+from instance.config import app_config
+
+#from flask_sqlachemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
 from pprint import pprint
 
 app = Flask(__name__)
 
+config_name = os.getenv('APP_SETTINGS')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/todo_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///todo_db'
 #SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
 
 
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Users(db.Model):
     """
     Will store users 
     """
@@ -40,22 +46,43 @@ class Todo(db.Model):
 
 
 @app.route('/user', methods=['POST'])
-def create_user(self):
+def create_user():
     data = request.get_json()
     hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User(public_id=str(uuid.uuid4), name=data['name'], password=hashed_password, admin=False)
-    self.user.session.add(new_user)
-    self.user.session.commit()
+    new_user = Users(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
     
     return jsonify({"message": "New user has been created!"})
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
-    return ''
+    users = Users.query.all()
 
-@app.route('/user/<int:id>', methods=['GET'])
-def get_a_user(id):
-    return ''
+    output = [] # store results in this list from json
+    for user in users:
+        user_data = {} # dict to be populated with the iterations
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+
+
+    return jsonify({'users': output})
+
+@app.route('/user/<string:public_id>', methods=['GET'])
+def get_a_user(public_id):
+    user = Users.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+
+    return jsonify({'user': user_data})
 
 @app.route('/user/<int:id>', methods=['PUT'])
 def edit_user(id):
@@ -67,4 +94,4 @@ def delete_user(id):
 
 if __name__ =='__main__':
     app.run(debug=True)
-    database_connection = DatabaseConnection()
+    
