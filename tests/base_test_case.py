9fd79b6a4611e5c001c21bacdb0c212818515b65
gtsofa <jlsnyule@yahoo.com>
2018-06-20 01:03:15 +0300

    refactor base test case

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -6,26 +6,48 @@ import jwt
 from flask_testing import TestCase
 
 from app.v2.models import Users, Todo
-from config import Config, config_environments
+from config import Config
 from manage import app, db
+from config import app_config
+
+app.config.from_object(app.config["testing"])
 
 class BaseTestCase(TestCase):
     """
     Base configurations for the tests
     """
     def create_app(self):
-        app.config.from_object(config_environments['testing'])
+        """
+        return app
+        """
+        #app.config.from_object(config_environments['testing'])
         return app
 
     def setUp(self):
+        """
+        create test data and set up test client
+        """
         self.client = self.app.testing_client()
-        db.drop_all()
         db.create_all()
-
+        
         # add a test user to the db
-        user = Users("TestUser", "password")
-        db.session.add()
+        user = Users("TestUser", "testpassword")
+        todo1 = Todo(complete=False, text="Create a Youtube video", created_by=1)
+        todo2 = Todo(complete=False, text="I want to go shopping", created_by=1)
+
+        db.session.add(user)
+        db.session.add(todo1)
+        db.session.add(todo2)
         db.session.commit()
 
         # generate token
-        
+
+    def tearDown(self):
+        """
+        Destroy test data
+        """
+        db.session.remove()
+        db.drop_all()
+
+if __name__ =='__main__':
+    nose.run()
